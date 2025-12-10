e8807b857fb32e186ccf6780e95235548072498f
alphataubio <alphataubio@gmail.com>
2025-12-10 15:10:55 -0500

    Update conf.py

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -83,7 +83,6 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options
 html_theme_options = {
     'logo_only': False,
-    'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'style_nav_header_background': '#2980B9',
